b6bd2d8b7e7f9f4e9a29ba84657ecc0e59ef8064
blackknight75 <devbox4714@gmail.com>
2017-01-24 15:36:41 -0700

    added route for all robots

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,10 +1,11 @@
 require_relative '../models/robot.rb'
+require 'sinatra'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
 end
-
