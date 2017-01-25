a221a933091df3be7af00dc9bb1664badf36e293
blackknight75 <devbox4714@gmail.com>
2017-01-24 23:07:45 -0700

    New robot is working. Show page layout finished. All robot layout finished.

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,8 +4,27 @@ require 'sinatra'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    "Welcome to Robot World"
+  end
+
   get '/robots' do
     @robots = Robot.all
     erb :index
+  end
+
+  get '/robots/new' do
+    erb :new_robot
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
   end
 end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 
 class Robot
-attr_reader :name, :city, :state, :department
+attr_reader :name, :city, :state, :department, :id
   def initialize(robot_params)
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
@@ -9,12 +9,28 @@ attr_reader :name, :city, :state, :department
     @city = robot_params["city"]
     @state = robot_params["state"]
     @department = robot_params["department"]
+    @id = robot_params["id"] if robot_params["id"]
   end
 
   def self.all
-    robots = @database.execute("SELECT * FROM robots")
+    robots = database.execute("SELECT * FROM robots")
     robots.map do |robot|
       Robot.new(robot)
     end
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    Robot.new(robot)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
   end
 end
